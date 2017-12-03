c9593ecdee42f8155818b14e50e530dbcd9237c3
Dominic Roy-Stang <dominicroystang@gmail.com>
2017-12-03 16:00:05 -0500

    Added a sample 'Hello World' contract.

---
+++ HelloWorldContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.0
+
+contract HelloWorldContract {
+    string public word = "Hello World";
+    /*
+     * this is bad practice because anyone who has access to our
+     * smart contract could get the world value.
+     */
+}
