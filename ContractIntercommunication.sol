7e5c9d43def8f6c8ab056a206e3c66cad9d3764d
Dominic Roy-Stang <dominicroystang@gmail.com>
2017-12-07 00:46:52 -0500

    Added a sample contract intercommunication example.

---
+++ ContractIntercommunication.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.19;
+
+contract CallerContract {
+    CalledContract toBeCalled = new CalledContract();
+    // for an already deployed contract, use the following syntax:
+    //CalledContract toBeCalled = CalledContract(0xAddressOfContract);
+    //Note that you still need the source code of the called contract to be able to call it.
+
+    function getNumber() constant returns(uint) {
+        return toBeCalled.getNumber();
+    }
+
+    function getWords() constant returns(bytes32) {
+        return toBeCalled.getWords();
+    }
+}
+
+contract CalledContract {
+    uint number = 64;
+    bytes32 words = "Hello World";
+    //string words = "Hello World"; // strings cannot be returned to other contracts. Use bytes32 instead.
+
+    function getNumber() constant returns(uint) {
+        return number;
+    }
+
+    function setNumber(uint _number) {
+        number = _number;
+    }
+
+    function getWords() constant returns(bytes32) {
+        return words;
+    }
+}
